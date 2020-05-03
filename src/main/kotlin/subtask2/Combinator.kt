b47543e3e32761fd5c9550e6efec8ab6c3465eb5
Olga Chaiko <chaiko.olga@gmail.com>
2020-05-04 00:01:33 +0300

    add subtask2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,34 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        val poster = array[0]
+        val color = array[1]
+        var result: Long
+        var quantity = 0
+        var i = 1
+
+        while (i <= color) {
+            result = factorial(color) / ((factorial(color - i)) * factorial(i))
+            if (result >= poster) {
+                quantity = i
+                break
+            }
+            i++
+        }
+        if (quantity == 0) {
+            return null
+        } else {
+            return quantity
+        }
+    }
+
+    fun factorial(n: Int): Long {
+        var result: Long = 1
+        for (i in 1..n) {
+            result *= i
+        }
+        return result
     }
 }
--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,11 @@
 package subtask3
 
+
+
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var result =0
+        return result
     }
 }
